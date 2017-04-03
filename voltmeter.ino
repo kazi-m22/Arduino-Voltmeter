0ef53b1de3519c4a4caf49b73669ce636b18ac1b
Kazi Mejbaul Islam <222mzb@gmail.com>
2017-04-03 20:07:15 +0000

    Add files via upload
    Only for under 5 volts(without relay)

---
+++ voltmeter.ino
@@ -0,0 +1,39 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float x,in_v;
+ 
+  x = analogRead(A0);
+  in_v = x*(5/1024.0);
+  
+  char c;
+  c = Serial.read();
+  if(c == 't')
+  {
+    if(in_v * 100 <1)
+  {
+    Serial.print(in_v*1000, DEC);
+    Serial.print(" milivolt");
+    Serial.println(" ");
+  }
+  else if( in_v * 100000 < 1)
+  {
+    Serial.print(in_v*100000, DEC);
+    Serial.print(" micro volt");
+    Serial.println(" ");
+  }
+  else
+  {
+    Serial.print(in_v, DEC);
+    Serial.print(" Volt");
+    Serial.println(" ");
+  }
+  
+  }
+
+}
